24f40ad8839f72b5d70335f90e9910a8d858eb68
th3gstv <gstvoaugusto@gmail.com>
2025-05-10 22:06:30 -0300

    adding

---
+++ antecessor-sucessor.py
@@ -0,0 +1,5 @@
+print("Este programa calcula o antecessor e o sucessor do número digitado!\n")
+n1 = int(input("Digite um número inteiro: "))
+n0 = n1 - 1
+n2 = n1 + 1
+print(f'Antecessor: {n0} | Número escolhido: {n1} |Sucessor: {n2}')
