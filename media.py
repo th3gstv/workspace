f6d3319b371c3adff793cbdaf37932417dce938d
th3gstv <gstvoaugusto@gmail.com>
2025-05-10 22:30:21 -0300

    adding

--- media.py
+++ media.py
@@ -1,4 +1,4 @@
 n1 = int(input("Primeira nota: "))
 n2 = int(input("Segunda nota: "))
 n3 = (n1+n2)/2
-print(f'A média aritmética entre {n1} e {n2} resulta em {n3}')
+print(f'A média aritmética entre {n1} e {n2} resulta em {n3:.2f}')
